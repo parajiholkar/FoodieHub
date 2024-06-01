66505338d072ac3ce7680e4dce7d3875ce7a78d5
Parajiholkar <parajiholkar81@gmail.com>
2024-06-02 02:32:49 +0530

    first commit

---
+++ app/src/main/java/com/project1/foodiehub/MainActivity.java
@@ -0,0 +1,20 @@
+package com.project1.foodiehub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project1.beehealthy.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
